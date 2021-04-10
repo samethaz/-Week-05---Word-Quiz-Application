88f5719b0dfdf7e7c251c73f19fa57740ee5716c
samethaz@gmail.com <samethaz@gmail.com>
2021-04-10 09:54:42 +0200

    second

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import questionsApp from './component/QuestionsApp.js';
+
+function App() {
+  return (
+    <div className="App">
+   <QuestionsApp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/question.js
@@ -0,0 +1,28 @@
+
+const questions=[ 
+    { question:"lemon",
+      answerOption:[ 
+            { answer:"Zug" , isCorrect:false } ,
+            { answer:"zitrone" , isCorrect:true } ,
+            { answer:"velo" , isCorrect:false } ,
+            { answer:"carotte" , isCorrect:false } ,
+    ]
+    }, 
+    {   question:"Train",
+        answerOption:[ 
+      { answer:"Zug" , isCorrect:true } ,
+      { answer:"zitrone" , isCorrect:false } ,
+      { answer:"velo" , isCorrect:false } ,
+      { answer:"carotte" , isCorrect:false } ,
+]
+    }, 
+    {   question:"Bisiklet",
+        answerOption:[ 
+      { answer:"Zug" , isCorrect:false } ,
+      { answer:"zitrone" , isCorrect:false } ,
+      { answer:"velo" , isCorrect:true } ,
+      { answer:"carotte" , isCorrect:false } ,
+]
+},]
+
+export default questions
